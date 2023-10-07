fd44514a1f02beff0e73e62793b3e889c2080f00
wenhaozhao <wenhaozhao@sohu-inc.com>
2023-10-07 23:01:36 +0800

    add mbc2

--- src/cartridge/mbc/mbc2.rs
+++ src/cartridge/mbc/mbc2.rs
@@ -3,20 +3,16 @@ use crate::cartridge::mbc::{BankMode, MBC};
 use crate::memory::Memory;
 use crate::Result;
 
-pub struct MBC1 {
+pub struct MBC2 {
     rom: Rom,
     ram: Ram,
-    ///
-    /// - BankMode   RAMBank   ROMBank
-    /// - 1 bit      2 bit     5 bit
-    ///
     bank: u8,
     ram_enable: bool,
 }
 
-impl MBC1 {
+impl MBC2 {
     pub fn new(rom_path: String, ram_path: String) -> Result<Self> {
-        Ok(MBC1 {
+        Ok(MBC2 {
             rom: Rom::new(rom_path)?,
             ram: Ram::new(ram_path)?,
             bank: 0x01,
@@ -35,10 +31,7 @@ impl MBC1 {
     }
 
     fn rom_bank_index(&self) -> u16 {
-        let index = match self.bank_mode() {
-            BankMode::ROM => self.bank & 0b0_11_11111,
-            BankMode::RAM => self.bank & 0b0_00_11111,
-        };
+        let index = self.bank & 0x0F;
         index as u16
     }
 
@@ -58,19 +51,19 @@ const ROM_BANK_SIZE: u16 = 0x4000;
 const ROM_0_BASE: u16 = 0x0000;
 /// rom_0 0x0000 - 0x3FFF
 const ROM_0_END: u16 = ROM_0_BASE + ROM_BANK_SIZE - 1;
-/// rom_01-7F 0x4000 - 0x7FFF
+/// rom_01-0F 0x4000 - 0x7FFF
 const ROM_X_BASE: u16 = 0x4000;
-/// rom_01-7F 0x4000 - 0x7FFF
+/// rom_01-0F 0x4000 - 0x7FFF
 const ROM_X_END: u16 = ROM_X_BASE + ROM_BANK_SIZE - 1;
 
-/// ram 8KB
-const RAM_BANK_SIZE: u16 = 0x2000;
-/// ram_00-03 0xA000 - 0xBFFF
+/// ram 512 * 4 Bits
+const RAM_BANK_SIZE: u16 = 0x200;
+/// ram 0xA000 - 0xA1FF
 const RAM_X_BASE: u16 = 0xA000;
-/// ram_00-03 0xA000 - 0xBFFF
+/// ram 0xA000 - 0xA1FF
 const RAM_X_END: u16 = RAM_X_BASE + RAM_BANK_SIZE - 1;
 
-impl Memory for MBC1 {
+impl Memory for MBC2 {
     fn read(&self, adr: u16) -> u8 {
         match adr {
             ROM_0_BASE..=ROM_0_END => {
@@ -82,8 +75,8 @@ impl Memory for MBC1 {
             }
             RAM_X_BASE..=RAM_X_END => {
                 if self.ram_enable {
-                    let adr = self.ram_bank_index() * RAM_BANK_SIZE + adr - RAM_X_BASE;
-                    self.ram.read(adr)
+                    let v = self.rom.read(adr - RAM_X_BASE);
+                    v & 0x0F // 由于数据由 4 Bits 组成, 因此该存储区中只有每个字节的低 4 位才会被使用
                 } else {
                     0x00
                 }
@@ -98,31 +91,24 @@ impl Memory for MBC1 {
         match offset {
             0x0000..=0x1FFF => {
                 // RAM 启用/禁用标志
-                let after = (value & 0x0A) == 0x0A;
-                if !after && !(after && self.ram_enable) {
-                    // ram access: enable -> disable
-                    self.ram.dump();
+                // 只有高位地址字节的最低有效位为零才能启用/关闭 RAM
+                if offset & 0x0100 == 0x0000 {
+                    let after = (value & 0x0A) == 0x0A;
+                    if !after && !(after && self.ram_enable) {
+                        // ram access: enable -> disable
+                        self.ram.dump();
+                    }
+                    self.ram_enable = after;
                 }
-                self.ram_enable = after;
             }
             0x2000..=0x3FFF => {
-                // Bank Number 第 0-4 位
-                let value = value & 0b0_00_11111;
-                let value = match value {
-                    0x00 | 0x20 | 0x40 | 0x60 => value | 0x01,
-                    _ => value
-                };
-                self.bank = (self.bank & 0b1_11_00000) | value;
-            }
-            0x4000..=0x5FFF => {
-                // Bank Number 第 5-6 位
-                let value = (value & 0b0000_0011) << 5;
-                self.bank = (self.bank & 0b1_00_11111) | value;
-            }
-            0x6000..=0x7FFF => {
-                // Bank 模式选择
-                let value = (value & 0b0000_0001) << 7;
-                self.bank = self.bank | value;
+                // Bank Number 第 0-3 位
+                // 只有写入地址的高位字节的最低有效位为 1 才能正确写入
+                if offset & 0x0100 == 0x0100 {
+                    // 数值的低 4 位将作为当前的 ROM Bank Number
+                    let value = value & 0x0F;
+                    self.bank = (self.bank & 0xF0) | value;
+                }
             }
             RAM_X_BASE..=RAM_X_END => {
                 if self.ram_enable {
@@ -137,7 +123,7 @@ impl Memory for MBC1 {
     }
 }
 
-impl MBC for MBC1 {}
+impl MBC for MBC2 {}
 
 #[cfg(test)]
 mod tests {
@@ -146,4 +132,3 @@ mod tests {
         todo!()
     }
 }
-
--- src/cartridge/mbc/mod.rs
+++ src/cartridge/mbc/mod.rs
@@ -1,6 +1,7 @@
 use crate::memory::Memory;
 
 pub mod mbc1;
+pub mod mbc2;
 
 enum BankMode {
     ROM,
