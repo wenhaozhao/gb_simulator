836946f2ba787f90956cf37df243ad34ef352b79
wenhaozhao <wenhaozhao@sohu-inc.com>
2023-10-08 11:45:08 +0800

    remove rom_only.rs

--- src/cartridge/mod.rs
+++ src/cartridge/mod.rs
@@ -5,7 +5,6 @@ use std::path::Path;
 use crate::memory::Memory;
 use crate::Result;
 
-pub mod rom_only;
 pub mod mbc;
 
 pub struct Rom {
@@ -75,6 +74,27 @@ pub trait Cartridge {}
 
 pub fn power_up(path: &Path) -> Box<dyn Cartridge> {
     todo!()
+}
+
+struct RomOnly {
+    rom: Rom,
+}
+
+impl RomOnly {
+    pub fn new(rom_path: String) -> Result<Self> {
+        let rom = Rom::new(rom_path)?;
+        Ok(RomOnly { rom })
+    }
+}
+
+impl Memory for RomOnly {
+    fn read(&self, i: u16) -> u8 {
+        self.rom.read(i)
+    }
+
+    fn write(&mut self, i: u16, v: u8) {
+        self.rom.write(i, v)
+    }
 }
 
 #[cfg(test)]
