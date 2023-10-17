04d3105661b13b9c1434bb0ea95b0acdf6640669
wenhaozhao <wenhaozhao@sohu-inc.com>
2023-10-17 13:52:10 +0800

    tmp

--- src/cpu/lr35902/opcode/cbprefixed_opcode.rs
+++ src/cpu/lr35902/opcode/cbprefixed_opcode.rs
@@ -2,6 +2,11 @@ use crate::cpu::lr35902::LR35902;
 
 impl LR35902 {
     pub fn cbprefixed_exec_opcode(&mut self, opcode: u8) -> u8 {
-        todo!()
+        match opcode {
+            0x00 => {
+
+            }
+            other => panic!(format!("Unsupported opcode:{}", other)),
+        }
     }
 }
